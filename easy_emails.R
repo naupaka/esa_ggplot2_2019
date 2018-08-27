d23429af0e0c4a2d148047404ea23b2034012fdc
Naupaka Zimmerman <naupaka@gmail.com>
2018-08-26 17:50:15 -0700

    Update email script

--- easy_emails.R
+++ easy_emails.R
@@ -1,4 +1,4 @@
-roster     <- read.csv("/Users/atredenn/Desktop/roster.csv")
+roster     <- read.csv("/Users/naupaka/Desktop/AM18_WK-9_(2018.0801).csv", skip = 2)
 emails     <- as.character(roster$Email)
 email_list <- paste(emails, collapse = ",")
 print(email_list)
